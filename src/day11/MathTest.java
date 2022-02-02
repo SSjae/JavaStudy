45d0cc03f5bf9f75c57e6c5cc3ed867a2f7e2341
SSjae <ssjj04022@naver.com>
2022-02-02 21:28:51 +0900

    Object, String, Math, Wrapper

--- src/day11/MathTest.java
+++ src/day11/MathTest.java
@@ -12,54 +12,30 @@ public class MathTest {
 		Random r = new Random();
 		System.out.println(r.nextInt(10) + 1); // : 1 ~ 10까지의 랜덤한 정수
 		
+		// Math.round(실수) : 소수점 첫째자리에서 반올림
+		double test = 21.1118090030;
+		System.out.println(Math.round(test));
+		// 넷째자리에서 반올림 하고 싶을 떄
+		// 1. 1000(10의 3승) 곱하기
+		// test * 1000 --> 21111.8090030
+		// 2. 반올림(Math.round())
+		// Math.round(21111.8090030) --> 21112
+		// 3. 다시 1000.0으로 나누기
+		// 21112 / 1000.0 --> 21.112
+		System.out.println( Math.round(test * 1000) / 1000.0 );
+
+		
+		// Math.ceil(실수) : 올림
+		System.out.println(Math.ceil(1.1));
+		// Math.floor(실수) : 내림
+		System.out.println(Math.floor(1.9999));
 		
 		
+		// Math.abs(값) : 절대값
+		System.out.println(Math.abs(-418974));
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		// Math.round(실수) : 소수점 첫째자리에서 반올림
-//		double test = 21.1118090030;
-//		System.out.println(Math.round(test));
-//		// 넷째자리에서 반올림 하고 싶을 떄
-//		// 1. 1000(10의 3승) 곱하기
-//		// test * 1000 --> 21111.8090030
-//		// 2. 반올림(Math.round())
-//		// Math.round(21111.8090030) --> 21112
-//		// 3. 다시 1000.0으로 나누기
-//		// 21112 / 1000.0 --> 21.112
-//		System.out.println( Math.round(test * 1000) / 1000.0 );
-		
-//		
-//		// Math.ceil(실수) : 올림
-//		System.out.println(Math.ceil(1.1));
-//		// Math.floor(실수) : 내림
-//		System.out.println(Math.floor(1.9999));
-//		
-//		
-//		// Math.abs(값) : 절대값
-//		System.out.println(Math.abs(-418974));
-//		
-//		
-//		// Math.pow(n,m) : n의 m제곱
-//		System.out.println(Math.pow(10, 3));
+		// Math.pow(n,m) : n의 m제곱
+		System.out.println(Math.pow(10, 3));
 	}
 }
